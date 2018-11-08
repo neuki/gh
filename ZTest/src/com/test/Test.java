8161095fac539b4238bcf85060bb6520c8ec6c5b
neunerkl <neunerkl@LAPTOP100.softvision.local>
2018-11-08 12:54:16 +0100

    tst

--- ZTest/src/com/test/Test.java
+++ ZTest/src/com/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Mein Test");
         System.out.println("Mein Test2");
+        System.out.println("Mein Test3");
     }
     
 }

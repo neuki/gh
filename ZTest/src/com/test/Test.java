9a028531dbf244ca2634430545e1a5a69b7d63fb
neunerkl <neunerkl@LAPTOP100.softvision.local>
2018-11-08 13:48:12 +0100

    tst

--- ZTest/src/com/test/Test.java
+++ ZTest/src/com/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Mein Test");
-        System.out.println("Mein Test2");
+        System.out.println("Mein Test99");
         System.out.println("Mein Test3");
         System.out.println("Mein Test5");
         System.out.println("Mein Test5");

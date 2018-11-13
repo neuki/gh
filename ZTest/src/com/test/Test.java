2709b180a61a14044c34ac7ca67409ed4d02900b
neunerkl <neunerkl@LAPTOP100.mshome.net>
2018-11-13 15:19:05 +0100

    tst

--- ZTest/src/com/test/Test.java
+++ ZTest/src/com/test/Test.java
@@ -3,20 +3,10 @@ package com.test;
 public class Test {
 
     public static void main(String[] args) {
-    	System.out.println("Mein Test88");
-    	System.out.println("Mein Test9977"); 
-    	System.out.println("Mein Test9955"); 
-    	System.out.println("Mein Test9966"); 
-
-        System.out.println("Mein Test99");
-        System.out.println("Mein Test3");
-        System.out.println("Mein Test5");
-        System.out.println("Mein Test8");
-        
-        System.out.println("Mein Test8899");
-        System.out.println("Mein Test8811");
-        System.out.println("Mein Test66666");
-        System.out.println("Testi");
+    	
+    	
+    	
+    	
     }
     
 }

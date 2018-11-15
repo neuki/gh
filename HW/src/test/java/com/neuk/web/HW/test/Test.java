de4055bd6cdcf68ce73c7afc622b83943ac5f268
neunerkl <neunerkl@softvision.local>
2018-11-15 17:25:57 +0100

    tst

--- HW/src/test/java/com/neuk/web/HW/test/Test.java
+++ HW/src/test/java/com/neuk/web/HW/test/Test.java
@@ -4,6 +4,4 @@ public class Test {
 
     @org.junit.Test
     public void test() throws Exception {
-       System.out.println("Right10"); 
-       System.out.println("Left1-Left");
-    }
+       System.out.println("Right10"); }

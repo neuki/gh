9eae8270003215788f1217b32637f745c11f4612
neunerkl <neunerkl@donat-group.de>
2018-11-15 16:50:34 +0100

    tst

--- HW/src/test/java/com/neuk/web/HW/test/Test.java
+++ HW/src/test/java/com/neuk/web/HW/test/Test.java
@@ -4,11 +4,9 @@ public class Test {
 
     @org.junit.Test
     public void test() throws Exception {
-<<<<<<< HEAD
        System.out.println("Right2"); 
-=======
        System.out.println("Left3"); 
->>>>>>> b1806a1a13c333a01adca9171e425c5ba8b3dd3c
+
     }
 
 }
